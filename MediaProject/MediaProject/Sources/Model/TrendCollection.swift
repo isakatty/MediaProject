b686774cea92be30d33bfc4c673f9bd2877c29bc
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-07-12 20:11:26 +0900

    [Remove] Legacy file 및 code 삭제

---
+++ MediaProject/MediaProject/Sources/Model/TrendCollection.swift
@@ -0,0 +1,14 @@
+//
+//  TrendCollection.swift
+//  MediaProject
+//
+//  Created by Jisoo Ham on 6/11/24.
+//
+
+import Foundation
+
+struct TrendCollection {
+    var actorInfo: [CastResponseDTO]?
+    var poster: [PosterPath]?
+    var similar: [TrendInfo]?
+}
